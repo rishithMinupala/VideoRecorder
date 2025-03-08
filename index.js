40436dde3a754214909e8555781c3d4e97606611
Rishith Minupala <sriramrishith@gmail.com>
2025-03-08 20:50:23 +0530

    added filters

--- index.js
+++ index.js
@@ -4,20 +4,22 @@ let recodrBtn = document.getElementById("record-btn");
 let captureCont = document.querySelector(".capture-cont");
 let captureBtn = document.querySelector(".capture-btn");
 let filtersCont = document.querySelector(".filters-cont");
+let filterVideo = document.querySelectorAll(".filter-video");
 let timer = document.querySelector(".timer");
-
+let previousFilter = filterVideo[0];
+let selectedFilter = filterVideo[0];
+let filterApplied = "none";
 let recorder;
 let chunks = [];
 let isRecording = false;
 let timerId;
 let secondsCounter = 0;
+let stream = null;
 
 const userData = {
-  //video: { frameRate: { ideal: 60 } },
+  video: { frameRate: { ideal: 60 } },
   audio: false,
 };
-
-
 
 //timer function
 const counter = () => {
@@ -46,12 +48,39 @@ const stopTimer = () => {
   secondsCounter = 0;
 };
 
+const applyFilter = (eachFilter) => {
+  previousFilter = selectedFilter;
+  selectedFilter = eachFilter;
+  previousFilter.parentElement.classList.remove("selected");
+  selectedFilter.parentElement.classList.add("selected");
+  filterApplied = getComputedStyle(eachFilter).getPropertyValue("filter");
+  video.style.filter = filterApplied;
+};
+
 //video stream function
 const getVideo = async () => {
-  let stream = await navigator.mediaDevices.getUserMedia(userData);
-  video.srcObject = stream;
-
-  recorder = new MediaRecorder(stream);
+  try {
+    stream = await navigator.mediaDevices.getUserMedia(userData);
+    video.srcObject = stream;
+    applyFilter(selectedFilter);
+    filterVideo.forEach((e) => {
+      e.srcObject = stream;
+    });
+  } catch (e) {}
+};
+const getStream = () => {
+  let canvas = document.createElement("canvas");
+  const drawFrame = () => {
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+    let ctx = canvas.getContext("2d");
+    ctx.filter = filterApplied;
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(drawFrame);
+  };
+  drawFrame();
+  let recordStream = canvas.captureStream(30);
+  let recorder = new MediaRecorder(recordStream);
 
   recorder.onstart = (e) => {
     chunks = [];
@@ -67,33 +96,40 @@ const getVideo = async () => {
     a.download = "stream.mp4";
     a.click();
   };
+  return recorder;
+};
 
-  recodrCont.onclick = (e) => {
-    if (!recorder) return;
-    isRecording = !isRecording;
-    if (isRecording) {
-      recorder.start();
-      startTimer();
-      recodrBtn.classList.remove("record-btn");
-      recodrBtn.classList.add("recording-btn");
-      recodrBtn.classList.add("scale-record");
-    } else {
-      recorder.stop();
-      stopTimer();
-      recodrBtn.classList.remove("scale-record");
-      recodrBtn.classList.remove("recording-btn");
-      recodrBtn.classList.add("record-btn");
-    }
-  };
+recorder = getStream();
+
+recodrCont.onclick = (e) => {
+  if (!recorder) return;
+  isRecording = !isRecording;
+  if (isRecording) {
+    setTimeout(() => recorder.start(), 500);
+    startTimer();
+    recodrBtn.classList.replace("record-btn", "recording-btn");
+    recodrBtn.classList.add("scale-record");
+  } else {
+    recorder.stop();
+    stopTimer();
+    recodrBtn.classList.remove("scale-record");
+    recodrBtn.classList.replace("recording-btn", "record-btn");
+  }
 };
 
 captureCont.onclick = (e) => {
+  if (stream == null) {
+    alert("Camera Access Required");
+    return;
+  }
   captureBtn.classList.add("scale-capture");
   let canvas = document.createElement("canvas");
   canvas.width = video.videoWidth;
   canvas.height = video.videoHeight;
   let ctx = canvas.getContext("2d");
+  ctx.filter = filterApplied;
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+  //console.log(filterApplied);
   let imageUrl = canvas.toDataURL("image/png");
 
   let a = document.createElement("a");
@@ -109,5 +145,11 @@ captureCont.onclick = (e) => {
 //navigator.mediaDevices.getUserMedia(userData).then((live) => {
 //  video.srcObject = live;
 //});
+//console.log(filterVideo);
+filterVideo.forEach((eachFilter) => {
+  eachFilter.onclick = () => {
+    applyFilter(eachFilter);
+  };
+});
 
 getVideo();
